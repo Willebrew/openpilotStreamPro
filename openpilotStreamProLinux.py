a3971abf87a877e07166cd61b8ce164411326752
Will <killebrewboys@me.com>
2025-01-14 00:30:21 -0700

    Add files via upload

---
+++ openpilotStreamProLinux.py
@@ -0,0 +1,42 @@
+import subprocess
+import platform
+
+rtmp_url = 'rtmp://a.rtmp.youtube.com/live2/77f4-a1uu-cgx3-0zbc-99cf'
+
+os_type = platform.system()
+
+if os_type != "Linux":
+    raise EnvironmentError("This script is configured for Linux (Ubuntu).")
+
+ffmpeg_command = [
+    'ffmpeg',
+    '-re',                                   # Read input at native frame rate
+    '-f', 'x11grab',                         # Screen capture for Linux
+    '-framerate', '30',                      # Supported framerate
+    '-video_size', '1920x1080',              # Set resolution to 1080p
+    '-i', ':0.0',                            # Display number (adjust if necessary)
+    '-f', 'lavfi',
+    '-i', 'anullsrc',                        # Add silent audio source
+    '-c:v', 'libx264',                       # Video codec
+    '-preset', 'veryfast',                   # Encoding preset
+    '-b:v', '2500k',                         # Set video bitrate to 2500 Kbps
+    '-maxrate', '2500k',                     # Max video bitrate
+    '-bufsize', '5000k',                     # Buffer size for rate control
+    '-pix_fmt', 'yuv420p',                   # Pixel format
+    '-g', '60',                              # Keyframe interval
+    '-c:a', 'aac',                           # Audio codec
+    '-b:a', '128k',                          # Set audio bitrate to 128 Kbps
+    '-f', 'flv',                             # Output format
+    rtmp_url                                 # YouTube RTMP URL
+]
+
+print("Streaming your screen. Press Ctrl+C to stop.")
+
+process = subprocess.Popen(ffmpeg_command)
+
+try:
+    process.wait()
+except KeyboardInterrupt:
+    print("Stopping the stream...")
+    process.terminate()
+    process.wait()
